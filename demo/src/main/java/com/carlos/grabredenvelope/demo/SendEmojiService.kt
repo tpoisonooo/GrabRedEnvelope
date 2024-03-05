ce3eb82ce11d029a7d6db54759dae65fb5251f20
tpoisonooo <khj.application@aliyun.com>
2024-03-05 16:22:54 +0800

    feat(app): debug wechat passed

--- demo/src/main/java/com/carlos/grabredenvelope/demo/SendEmojiService.kt
+++ demo/src/main/java/com/carlos/grabredenvelope/demo/SendEmojiService.kt
@@ -2,19 +2,47 @@ package com.carlos.grabredenvelope.demo
 
 import android.accessibilityservice.AccessibilityService
 import android.os.Bundle
+import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.EditText
+import android.widget.TextView
 import com.carlos.cutils.util.LogUtils
+
+class NoDoubleClick {
+    var time = 0L
+
+    fun pass(): Boolean {
+        var now = System.currentTimeMillis()
+        if (time == 0L) {
+            time = now
+            return true
+        }
+
+        if (now - time > 1000) {
+            time = now
+            return true
+        }
+        return false
+    }
+}
 
 /**
  * Created by Carlos on 2021/2/8.
  * 自动发送表情脚本, 基于微信8.0.0
  */
 class SendEmojiService : AccessibilityService() {
+    private var residGroup = "com.tencent.mm:id/obn"
+    private var residUsername = "com.tencent.mm:id/brc"
+    private var residContent = "com.tencent.mm:id/bkl"
+    private var residEditText = "com.tencent.mm:id/bkk"
+    private var myname = "茴香豆"
+    private var groupname = ""
+    private var lastusername = ""
+    private var lastcontent = ""
+    private var firststart = 0L
+    private var throttle = NoDoubleClick()
 
-    private var windowClassName = ""
-    private val CHAT_ACTIVITY = "com.tencent.mm.ui.LauncherUI" //微信红包弹框
 
     override fun onCreate() {
         super.onCreate()
@@ -29,51 +57,127 @@ class SendEmojiService : AccessibilityService() {
             }
             AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED -> {
                 LogUtils.d("界面改变:$event")
-                windowClassName = event.className.toString()
-                sendMessage()
-
             }
             AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED -> {
                 LogUtils.d("内容改变:$event")
+                // extract latest event in framelayout
+                parseSendMessage(event)
             }
         }
     }
 
-    /**
-     * 找到文本框输入表情，找到发送按钮点击循环执行
-     */
-    private fun sendMessage() {
-        if (windowClassName != CHAT_ACTIVITY) return
-        val accessibilityNodeInfo =
-            rootInActiveWindow?.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/auj")
-                ?: return
+    private fun parseSendMessage(event: AccessibilityEvent) {
+        var classname = event.className.toString()
+        var packagename = event.packageName.toString()
+        if (classname != "android.widget.LinearLayout" && classname != "android.widget.FrameLayout") return
+        if (packagename != "com.tencent.mm") return
 
-        for (editText in accessibilityNodeInfo) {
-            if (editText.className == EditText::class.java.name) {
-                val arguments = Bundle()
-                arguments.putCharSequence(
-                    AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE,
-                    "[烟花]"
-                )
-                editText.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments)
+        if (firststart == 0L) {
+            firststart = System.currentTimeMillis()
+            return
+        }
+        if (System.currentTimeMillis() - firststart < 1000 * 5) {
+            Log.d("msg", "skip first start time + 5s")
+            return
+        }
 
-                send()
+        // fetch group name
+        var nodeInfo = rootInActiveWindow.findAccessibilityNodeInfosByViewId(residGroup)
+        for (tv in nodeInfo) {
+            if (tv.className == TextView::class.java.name) {
+                var content = tv.text.toString()
+                groupname = content
                 break
-
             }
-
         }
 
-        sendMessage()
+        // fetch all messages
+        nodeInfo = rootInActiveWindow.findAccessibilityNodeInfosByViewId(residUsername)
+        if (nodeInfo.size < 1) {
+            Log.d("msg", "residUsername not found, return")
+            return
+        }
+        var send = false
+        var tv = nodeInfo.last()
+        if (tv.className == TextView::class.java.name) {
+            var username = tv.text.toString()
+            if (!lastusername.equals(username)) {
+                lastusername = username
+                send = true
+            }
+        }
 
+        nodeInfo = rootInActiveWindow.findAccessibilityNodeInfosByViewId(residContent)
+        tv = nodeInfo.last()
+        if (tv.className == TextView::class.java.name) {
+            var content = tv.text.toString()
+            if (!lastcontent.equals(content)) {
+                lastcontent = content
+                send = true
+            }
+        }
+
+        Log.d("msg", groupname)
+        Log.d("msg", lastusername)
+        Log.d("msg", lastcontent)
+
+        if (lastusername.equals(myname)) {
+            send = false
+        }
+        Log.d("msg", send.toString())
+        if (send) {
+            nodeInfo = rootInActiveWindow.findAccessibilityNodeInfosByViewId(residEditText)
+            if (nodeInfo.size > 0) {
+                for (et in nodeInfo) {
+                    if (et.className == EditText::class.java.name) {
+                        var args = Bundle()
+                        args.putCharSequence(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, lastcontent)
+                        et.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, args)
+                        Thread.sleep(1000)
+
+                        var do_send = send()
+                        Log.d("msg", "action send?")
+                        Log.d("msg", do_send.toString())
+                    }
+                }
+
+            }
+        }
+
+//        val accessibilityNodeInfo =
+//            rootInActiveWindow?.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/auj")
+//                ?: return
+//
+//        for (editText in accessibilityNodeInfo) {
+//            if (editText.className == EditText::class.java.name) {
+//                val arguments = Bundle()
+//                arguments.putCharSequence(
+//                    AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE,
+//                    "[烟花]"
+//                )
+//                editText.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments)
+//
+//                send()
+//                break
+//
+//            }
+//
+//        }
     }
 
-    private fun send() {
+    private fun send(): Boolean {
+        if (!throttle.pass()) {
+            Log.d("msg", "click too more")
+            return false
+        }
+        var do_send = false
         val accessibilityNodeInfos =
-            rootInActiveWindow?.findAccessibilityNodeInfosByText("发送") ?: return
+            rootInActiveWindow?.findAccessibilityNodeInfosByText("发送") ?: return false
         for (accessibilityNodeInfo in accessibilityNodeInfos) {
+            do_send = true
             accessibilityNodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK)
         }
+        return do_send
     }
 
 
